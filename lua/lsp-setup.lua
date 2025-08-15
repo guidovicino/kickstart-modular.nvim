a97c73959b13ce082f34a712eb9a4774d456af81
Guido Vicino <mail@guidovicino.com>
2025-08-15 10:13:09 +0200

    fix(lsp): update mason-lspconfig setup for v2 API

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -109,19 +109,22 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
 
+
+-- Updated mason-lspconfig setup for v2 API
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
+
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+        filetypes = (servers[server_name] or {}).filetypes,
+      }
+    end,
+  },
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
 
 -- vim: ts=2 sts=2 sw=2 et
